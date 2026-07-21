5213c84740baeb481169c48ebf5eb0228245d29e
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2026-07-21 22:08:26 +0530

    remove-duplicates-from-sorted-array

---
+++ Easy/RemoveDuplicateFromIntArray/RemoveDuplicateFromIntArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateFromIntArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var result = RemoveDuplicates(nums);
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int previousNum = 0;
+            List<int> temp = new List<int>(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNum = nums[i];
+
+                if (i > 0)
+                {
+                    previousNum = nums[i - 1];   
+                }
+
+                if (currentNum == previousNum && i>0)
+                {
+                    temp.Remove(currentNum);
+                }
+            }
+
+            for (int i = 0; i < temp.Count; i++)
+
+            {
+                nums[i] = temp[i];
+            }
+
+            return temp.Count;
+        }
+    }
+}
